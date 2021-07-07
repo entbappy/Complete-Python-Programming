3e193c566700ab25c7576b0e2069c38384dffe3a
Bappy Malik <bappymalik4161@gmail.com>
2021-07-08 01:52:33 +0600

    initial

---
+++ tut3.py
@@ -0,0 +1,2 @@
+import sklearn
+print("Hello world and welcome")
---
+++ tut6.py
@@ -0,0 +1,15 @@
+#This is the code for subscrbing
+# print("Subscribe to DSwithBappy")
+#
+# """
+# Python is an interpreted high-level general-purpose programming language. Python''
+# 's design philosophy emphasizes code readability with its notable use of significant '
+# 'indentation. Wikipedia
+# sdhiofsd
+# sdhfldsjfl
+# """
+#
+# print("Subscribe to to my channel")  #This is the code for my channel
+
+print("\"bappy")
+
