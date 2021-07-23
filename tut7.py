6e8cf13793f8c0e583e43cd02a9a396d07325ccb
Bappy Ahmed <68845761+entbappy@users.noreply.github.com>
2021-07-23 19:35:38 +0600

    Add files via upload

---
+++ tut7.py
@@ -0,0 +1,49 @@
+# Varibale , Datatypes and Typecasting
+
+# print("Helloajshakshkahdkahdkaduasgdu")
+#
+# print("Helloajshakshkahdkahdkaduasgdu")
+
+# var = "gur"
+#
+# print(var)
+# print(var)
+# var1 = "Hello this bappy"
+# print(var1)
+# print(var1)
+# print(var1)
+
+# var2 = 33
+# var3 = 23
+# output = var2 + var3
+# print(output)
+# print(output)
+# print(output)
+
+# var4 = 4.5
+# var5 = 5
+#
+# out = var4 + var5
+# print(out)
+
+# var7 = 'Hello djskhfksdhfkjsdhfksd'
+# var8 = 8978969698798475983459439749385743
+# var9 = 67.6
+# var10 = (6+9j)
+# # print(type(var10))
+# print(type(var9))
+
+
+# var11 = 9.6
+# var12 = 67
+#
+# print(float(int(var11 + var12)))
+
+print(10*"Bappy \n")
+
+'''
+Quiz - Soved in video
+Exercise - Next video
+Projects - 
+'''
+
