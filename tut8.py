772d1ad25c85461bb0fe2239b9d74cf7c3065024
Bappy Ahmed <68845761+entbappy@users.noreply.github.com>
2021-08-06 01:07:46 +0600

    Add files via upload

---
+++ tut8.py
@@ -0,0 +1,13 @@
+mystring = "My name is Bappy"
+# print(mystring[-1:-5:-1])
+
+# print(mystring.isalnum())
+# print(mystring.isalpha())
+# print(mystring.endswith("t"))
+# print(mystring.count("B"))
+# print(mystring.capitalize())
+# print(mystring.find("is"))
+# print(mystring.lower())
+# print(mystring.upper())
+print(mystring.replace("My", "His"))
+
